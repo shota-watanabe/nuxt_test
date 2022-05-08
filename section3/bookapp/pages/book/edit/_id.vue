af732d4a569a7c5d47eb3d630e7cd39e123798c8
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-05-08 22:41:37 +0900

    beforeRouteEnter(ナビゲーションガード)

--- section3/bookapp/pages/book/edit/_id.vue
+++ section3/bookapp/pages/book/edit/_id.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
     book/edit/_id<br>
-    {{ $route.params.id }}
-    {{ books }}
-    {{ books[$route.params.id].title }}
+    {{ book.title }}
   </div>
 </template>
 
 <script>
 export default {
+  beforeRouteEnter(to, from, next){
+    next(vm=>{
+      vm.book = vm.books[vm.$route.params.id]
+    })
+  },
+
   props:{
     books: {
       type: Array,
       default: ()=>{}
     }
-  }
+  },
+  data(){
+    return{
+      book:''
+    }
+  },
 }
 </script>
 
